88188a75383834f1f88008d0eb2b130bf8e875c1
PramukaNavodh <pramunavodh@gmail.com>
2025-03-09 13:58:55 +0530

    few upgrades in Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,16 @@
-"user client";
+"use client";
+import HomeBanner from "@/components/HomeBanner";
 
-export default function Playerview() {
+export default function Home() {
+  const imagePaths = [
+    '/cricketbg.jpg',
+    '/cricketbga.jpg',
+    '/cricketbgb.jpg',
+    '/cricketbgc.jpg',
+  ];
   return (
-    <div>
+    <div className="bg-gray-900">
+      <HomeBanner images={imagePaths} interval={3000} />
     </div>
   );
 }
